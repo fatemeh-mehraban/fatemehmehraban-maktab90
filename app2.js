2f0bfeb0ebb4f14cab0383b1bf4f68c08836724b
mehraban <mehraban.fatemeh73@gmail.com>
2023-01-22 13:14:25 +0330

    creat function reset and read (3 && 4 function) for qustion 2

--- app2.js
+++ app2.js
@@ -4,8 +4,8 @@ function Increase(){
     Number = Number+1
     return Number
 }
-Increase()
-console.log(Increase());
+// console.log("Increase :" +Increase());
+
 function Decrease(){
     if(Number !== 0){
         Number = Number-1
@@ -14,4 +14,15 @@ function Decrease(){
     }
     return Number
 }
-// console.log(Decrease());
+// console.log("Decrease :" + Decrease());
+
+function Reset(){
+    Number = 0
+    return Number
+}
+// console.log(Reset());
+
+function Read(Number){
+  return console.log(Number);
+}
+Read("Read : " + Number);
